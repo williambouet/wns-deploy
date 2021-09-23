c691404f02f277fd5b519dd4d212d03790cc40f1
Thomas Culdaut <thomas.culdaut@gmail.com>
2021-09-23 10:43:20 +0200

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server";
+import { buildSchema } from "type-graphql";
+import mongoose from "mongoose";
+
+import { WilderResolver } from "./resolvers/wilder.resolver";
+
+(async () => {
+  await mongoose.connect("mongodb://localhost:27017/wilders", {useUnifiedTopology: true, useNewUrlParser: true});
+  const schema = await buildSchema({
+    resolvers: [WilderResolver],
+  });
+  const server = new ApolloServer({ schema });
+
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+})();
